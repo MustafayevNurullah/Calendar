a2ea1b493547fdd9f1abf620b4e559807ac948c0
nurlan3295 <nurlan3295@gmail.com>
2020-12-07 07:23:22 +0400

    12.07.2020

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -7,11 +7,11 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            
-                
+            while (true)
+            {
                 List<int> vs = new List<int>();
                 vs.Add(31);
-                vs.Add(29);
+                vs.Add(28);
                 vs.Add(31);
                 vs.Add(30);
                 vs.Add(31);
@@ -26,253 +26,152 @@ namespace Task3
                 string date1 = Console.ReadLine();
                 Console.WriteLine("Enter Date2");
                 string date2 = Console.ReadLine();
-                int day1 =
-                  DayConvert1(date1.Substring(0, 2));
-                int day2 =
+                int day1 = Convert.ToInt32(date1.Substring(0, 2));
+                int day2 = Convert.ToInt32(date2.Substring(0, 2));
+                int month1 = Convert.ToInt32(date1.Substring(2, 2));
+                int month2 = Convert.ToInt32(date2.Substring(2, 2));
 
-                DayConvert2(date2.Substring(0, 2));
-                int month1 =
-                   MonthConvert1(date1.Substring(2, 2));
-                int month2 =
-                MonthConvert2(date2.Substring(2, 2));
                 int year1 = Convert.ToInt32(date1.Substring(4));
                 int year2 = Convert.ToInt32(date2.Substring(4));
                 int okday = 0;
-
-
-                DaysCalculated();
-                void cal()
+                void yearsChange()
                 {
-
-                    #region 1year               
-                    int ab = 0;
-                    if (year1 + 1 == year2)
+                    if(year1==year2 & month1<month2)
                     {
-                        ab = 12;
+                        int tmp;
+                        tmp = month2;
+                        month2 = month1;
+                        month1 = tmp;
 
-                        for (int i = month1; i <= ab; i++)
-                        {
-                            okday += vs[i - 1];
-                            if (i == 12)
-                            {
-                                i = 0;
-                                ab = month2 - 1;
-                            }
-                        }
+                        tmp = day2;
+                        day2 = day1;
+                        day1 = tmp;
+                    }
+                   
+                    if (year1 < year2)
+                    {
+                        int tmp;
+                        tmp=year2;
+                        year2 = year1;
+                        year1 = tmp;
+
+                        tmp = month2;
+                        month2 = month1;
+                        month1 = tmp;
+
+                        tmp = day2;
+                        day2 = day1;
+                        day1 = tmp;
+
 
                     }
 
-                    if (year2 + 1 == year1)
-                    {
-
-                        ab = 12;
-
-                        for (int i = month2; i <= ab; i++)
-                        {
-                            okday += vs[i - 1];
-                            if (i == 12)
-                            {
-                                i = 0;
-                                ab = month1 - 1;
-                            }
-                        }
-
-
-
-                    }
-                    #endregion
-
-                    if (year1 == year2)
-                    {
-                        if (month1 > month2)
-                        {
-                            for (int i = month2; i < month1; i++)
-                            {
-                                okday += vs[i];
-
-                            }
-                        }
-                        else
-                        {
-                            for (int i = month1; i < month2; i++)
-                            {
-                                okday += vs[i];
-
-                            }
-                        }
-                    }
-
-                    if (year1 + 1 != year2 & year2 + 1 != year1 & year1 != year2)
-                    {
-                        if (month1 != month2)
-                        {
-                            if (year1 > year2)
-                            {
-
-
-                                year2++;
-
-                                for (int i = month2; i < 12; i++)
-                                {
-                                    okday += vs[i - 1];
-
-                                }
-                                for (int i = year2; i <= year1; i++)
-                                {
-                                    okday += 365;
-                                }
-
-                            }
-                            else
-                            {
-                                for (int i = month1; i < 12; i++)
-                                {
-                                    okday += vs[i - 1];
-                                   
-
-                                }
-                                year1++;
-                                for (int i = year1; i <= year2; i++)
-                                {
-                                    okday += 365;
-                                }
-                                //kod
-                            }
-                        }
-                        else
-                        {
-                            if (year1 > year2)
-                            {
-                                year1 += 1;
-
-                                for (int i = year2; i <= year1; i++)
-                                {
-                                    okday += 365;
-                                }
-                            }
-                            else
-                            {
-                                year1 += 1;
-                                for (int i = year1; i <= year2; i++)
-                                {
-                                    okday += 365;
-                                }
-                            }
-                        }
-
-                    }
-
-                }           
-          
+                }
+                yearsChange();
                 cal();
+                DaysCalculated();
+                ok();
+               void cal()
+                {
+                    if(year2+1==year1 & month1>month2)
+                    {
+                        MonthCalculted();
+                        okday += 365;
+                    }
+                    if (year2 + 1 == year1 & month1 < month2)
+                    {
+                        MonthCalcultted2();
+                        
+                    }
+                    if (year2 + 1 == year1 & month1 == month2)
+                        okday += 365;
+
+                    if(year1-year2>=2 )
+                    {
+                        if (month1 == month2)
+                        {
+                            int tmp = year1 - year2;
+                            okday += tmp * 365;
+                        }
+                        if (month1 < month2)
+                        {
+                            MonthCalcultted2();
+                           //modify
+                            year2++;
+                           int tmp = year1 - year2;
+                            year2--;
+                            okday += tmp * 365;
+                        }
+                       if(month1>month2)
+                        {
+                            MonthCalculted();
+                            //modify
+                            int tmp = year1 - year2;
+                            okday += tmp* 365;
+                        }
+
+                    }
+
+                    //03/01/2020
+                    //04/02/2020
+                    if (year1==year2)
+                    {
+                        MonthCalculted();
+                    }
+
+                }
+                void MonthCalculted()
+                {
+                    for (int i = month2; i < month1; i++)
+                    {
+                        okday += vs[i - 1];
+                    }
+                }
+
+                void MonthCalcultted2()
+                {
+                    int tmp = 12;
+                    for (int i = month2; i <= tmp; i++)
+                    {
+                        okday += vs[i - 1];
+                        if (i == 12)
+                        {
+                            i = 1;
+                            tmp = month1;
+                        }
+                    }
+                }
                 Console.WriteLine("Result day");
                 Console.WriteLine(okday.ToString());
-  
-                int DayConvert1(string tmp)
+                void ok()
                 {
-                    if (Convert.ToInt32(tmp.Substring(0, 1)) == 0)
+                        if(month1>2)
+                        {
+                            year1++;
+                        }
+                    if (year1 != year2)
                     {
-                        return Convert.ToInt32(tmp.Substring(1, 1));
+                        if (month2 <= 2)
+                        {
+                            year2--;
+                        }
                     }
-                    else
-                    {
-                        return Convert.ToInt32(tmp);
-                    }
-                }
-                int DayConvert2(string tmp)
-                {
-                    if (Convert.ToInt32(tmp.Substring(0, 1)) == 0)
-                    {
-                        return Convert.ToInt32(tmp.Substring(1, 1));
-                    }
-                    else
-                    {
-                        return Convert.ToInt32(tmp);
-                    }
-                }
-                int MonthConvert1(string tmp)
-                {
-                    if (Convert.ToInt32(tmp.Substring(0, 1)) == 0)
-                    {
-                        return Convert.ToInt32(tmp.Substring(1, 1));
-                    }
-                    else
-                    {
-                        return Convert.ToInt32(tmp);
-                    }
-
-
-
-                }
-                int MonthConvert2(string tmp)
-                {
-                    if (Convert.ToInt32(tmp.Substring(0, 1)) == 0)
-                    {
-                        return Convert.ToInt32(tmp.Substring(1, 1));
-                    }
-                    else
-                    {
-                        return Convert.ToInt32(tmp);
-                    }
-
-
-
+                    okday+= ((year1 ) / 4 - (year1 ) / 100 + (year1 ) / 400) - ((year2 ) / 4 - (year2) / 100 + (year2 ) / 400);
                 }
                 void DaysCalculated()
                 {
-                   
-
-                    if (year1 > year2)
+                    if (day1 > day2)
                     {
-                        okday -= day2 - 1;
-                        okday += day1 - 1;
+                        okday += day1 - day2;
                     }
                     else
                     {
-                        okday -= day1 - 1;
-                        okday += day2 - 1;
-                    }
-
-                }
-                void MonthCalculated()
-                {
-                    if (year1 != year2 & year1 > year2)
-                    {
-
-                        int a = 12;
-                        for (int i = month2; i <= a; i++)
-                        {
-                            okday += vs[i - 1];
-                            if (i == 12)
-                            {
-                                i = 0;
-                                a = month1 - 1;
-
-                            }
-
-                        }
-                    }
-                    else
-                    {
-                        int a = 12;
-                        for (int i = month1; i <= a; i++)
-                        {
-                            okday += vs[i - 1];
-                            if (i == 12)
-                            {
-                                i = 0;
-                                a = month2 - 1;
-
-                            }
-
-                        }
+                        okday -= day2 - day1;
                     }
                 }
-
             }
-        
 
-        
-        }
+             }
+         }
     }
 
